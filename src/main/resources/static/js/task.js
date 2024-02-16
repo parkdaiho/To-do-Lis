d3214d44a3f45c73c44a71c5e8c3dc1d3cbd9762
parkdaiho <eogh7204@gmail.com>
2024-02-16 19:32:02 +0900

    check task test completed.

--- src/main/resources/static/js/task.js
+++ src/main/resources/static/js/task.js
@@ -1,6 +1,6 @@
 const addBtn = document.getElementById("add-btn");
 
-if(addBtn) {
+if (addBtn) {
     addBtn.addEventListener("click", () => {
         fetch("/api/task", {
             method: "POST",
@@ -11,7 +11,7 @@ if(addBtn) {
                 contents: document.getElementById("input-box").value,
             }),
         }).then(response => {
-            if(response.ok) {
+            if (response.ok) {
                 location.reload();
             }
         });
@@ -20,23 +20,38 @@ if(addBtn) {
 
 const deleteBtns = document.getElementsByClassName("delete-btn");
 
-if(deleteBtns) {
-    for(let i = 0; i < deleteBtns.length; i++) {
+if (deleteBtns) {
+    for (let i = 0; i < deleteBtns.length; i++) {
         let id = deleteBtns[i].getAttribute("id").substring(5);
-        console.log(id);
 
         deleteBtns[i].addEventListener("click", () => {
             fetch("/api/task/" + id, {
-            method: "DELETE",
+                method: "DELETE",
                 headers: {
-                "Content-type": "application/json",
-            },
-        }).then(response => {
-            if(response.ok) {
-                location.reload();
+                    "Content-type": "application/json",
+                },
+            }).then(response => {
+                if (response.ok) {
+                    location.reload();
+                }
+            });
+
+        });
+    }
+}
+
+const list = document.getElementsByTagName("li");
+
+if (list) {
+    for (let i = 0; i < list.length; i++) {
+        let li = list[i];
+
+        li.addEventListener("click", () => {
+            if(li.getAttribute("class") === "checked") {
+                li.setAttribute("class", "");
+            } else {
+                li.setAttribute("class", "checked");
             }
         });
-
-        })
     }
 }
